f133d3da9166aca41a9e242178a3d5450bdf673b
Nazar Archakov <nazar.archakov222@gmail.com>
2021-02-04 22:37:48 +0300

    feat: add decision

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
+exports.min = function min(array) {
+    if (array === undefined || array[0] === undefined) {
+        return 0;
+    }
 
-exports.min = function min (array) {
-  return 0;
-}
+    let min = Math.min(...array);
+    return min;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array === undefined || array[0] === undefined) {
+        return 0;
+    }
 
-exports.avg = function avg (array) {
-  return 0;
-}
+    let max = Math.max(...array);
+    return max;
+};
+
+exports.avg = function avg(array) {
+    if (array === undefined || array[0] === undefined) {
+        return 0;
+    }
+
+    let sum = 0;
+    array.forEach((item) => {
+        sum += item;
+    });
+
+    return sum / array.length;
+};
